6e2467148467cbc934ba825e2697d992575c22cb
thrashches <thrashches@yandex.ru>
2025-06-17 01:53:21 +0300

    Fix paths for pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     basePath: "/pa-mir-next",
-    assetPrefix: "pa-mir-next",
     trailingSlash: true,
     output: 'export',
     images: {
