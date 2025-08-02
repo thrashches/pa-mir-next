1779091348d0c3e5b1e137f2c75d5933e459af74
thrashches <thrashches@yandex.ru>
2025-08-02 15:12:10 +0300

    Corrections

--- src/components/sections/HelpWithTranslate/HelpWithTranslate.tsx
+++ src/components/sections/HelpWithTranslate/HelpWithTranslate.tsx
@@ -12,7 +12,7 @@ export default function HelpWithTranslate() {
             <div>
                 <article className="md:max-w-[600px] lg:max-w-[600px]">
                     <h2 className="md:text-4xl text-3xl text-left text-gray-900 font-bold">
-                        Получите помощь с переводом
+                        Остались вопросы?
                     </h2>
                     <p className="text-xl text-left text-gray-500 my-3">
                         Свяжитесь с нами для бесплатной консультации и узнайте, как мы можем помочь вам.
